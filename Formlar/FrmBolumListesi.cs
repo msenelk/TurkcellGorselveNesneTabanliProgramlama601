00731389867e13b07c0284f9d7c27d6e899ff178
Muhammed Kizilirmak <senel_1996@hotmail.com>
2025-01-13 21:50:21 +0300

    Veri tabanında yer alan bölümler listelendi.

---
+++ Formlar/FrmBolumListesi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TurkcellGorselveNesneTabanliProgramlama601.Entity;
+
+namespace TurkcellGorselveNesneTabanliProgramlama601.Formlar
+{
+    public partial class FrmBolumListesi : Form
+    {
+        public FrmBolumListesi()
+        {
+            InitializeComponent();
+        }
+        OgrenciSinavEntities db=new OgrenciSinavEntities();
+        private void FrmBolumListesi_Load(object sender, EventArgs e)
+        {
+            var degerler = from x in db.TblBolum
+                           select new
+                           {
+                               x.BolumID,
+                               x.BolumAd
+                           };
+            dataGridView1.DataSource = degerler.ToList();
+        }
+    }
+}
