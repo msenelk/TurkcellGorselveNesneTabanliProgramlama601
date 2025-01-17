8d4d9f7fe4d98db6ebd55f785a93c5fb1841ebca
Muhammed Kizilirmak <senel_1996@hotmail.com>
2025-01-18 00:19:20 +0300

    Sınav notu bilgileri güncellendi.

--- Formlar/FrmNotlar.cs
+++ Formlar/FrmNotlar.cs
@@ -51,7 +51,7 @@ namespace TurkcellGorselveNesneTabanliProgramlama601.Formlar
             MessageBox.Show("Öğrenci Not Bilgisi İçin Sisteme Kaydedildi");
         }
 
-        private void btnHesapla_Click(object sender, EventArgs e)
+        private void OrtalamaHesapla() // Güncelleme sırasında hesapla butonuna basmamak için yeni br metot oluşturduk ve onu çağırıyoruz.
         {
             byte s1, s2, s3, q1, q2, proje;
             double ortalama;
@@ -61,9 +61,14 @@ namespace TurkcellGorselveNesneTabanliProgramlama601.Formlar
             s3 = Convert.ToByte(txtSinav3.Text);
             q1 = byte.Parse(txtQuiz1.Text);
             q2 = byte.Parse(txtQuiz2.Text);
-            proje=byte.Parse(txtProje.Text);
-            ortalama=(s1+s2+s3+q1+q2+proje)/ 7;
-            txtOrtalama.Text=ortalama.ToString(); 
+            proje = byte.Parse(txtProje.Text);
+            ortalama = (s1 + s2 + s3 + q1 + q2 + proje) / 7;
+            txtOrtalama.Text = ortalama.ToString();
+        }
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            OrtalamaHesapla();
+          
         }
 
         private void btnListele_Click(object sender, EventArgs e)
@@ -149,7 +154,18 @@ namespace TurkcellGorselveNesneTabanliProgramlama601.Formlar
 
         private void btnGuncelle_Click(object sender, EventArgs e)
         {
-
+            OrtalamaHesapla();
+            int id = int.Parse(txtID.Text);
+            var x = db.TblNotlar.Find(id);
+            x.Sinav1 = int.Parse(txtSinav1.Text);
+            x.Sinav2 = int.Parse(txtSinav2.Text);
+            x.Sinav3 = int.Parse(txtSinav3.Text);
+            x.Quiz1 = int.Parse(txtQuiz1.Text);
+            x.Quiz2 = int.Parse(txtQuiz2.Text);
+            x.Proje = int.Parse(txtProje.Text);
+            x.Ortalama = int.Parse(txtOrtalama.Text);
+            db.SaveChanges();
+            MessageBox.Show("Öğrenci Notları Başarılı Bir Şekilde Güncellendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
